80c7972639aff56322e69762803ab2f3c90e277a
Nikolai Degtiarev <NikAndDeg@yandex.ru>
2024-07-04 16:49:01 +0500

    Увеличил покрытие Branch до 0.67

---
+++ server/src/test/java/ru/practicum/shareit/request/model/ItemRequestTest.java
@@ -0,0 +1,28 @@
+package ru.practicum.shareit.request.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRequestTest {
+	private ItemRequest request1;
+	private ItemRequest request2;
+
+	@Test
+	void request_equals_test() {
+		LocalDateTime ldt = LocalDateTime.now();
+		request1 = createRequest(1, ldt);
+		request2 = createRequest(1, ldt);
+		assertTrue(request1.equals(request2));
+	}
+
+	private ItemRequest createRequest(int id, LocalDateTime ldt) {
+		ItemRequest request = new ItemRequest();
+		request.setId(id);
+		request.setDescription("description");
+		request.setCreated(ldt);
+		return request;
+	}
+}
